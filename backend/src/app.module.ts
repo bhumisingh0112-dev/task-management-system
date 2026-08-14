f6c86d134e77e8f5c8de3086d4f65ec51fc2e02a
bhumisingh0112-dev <bhumisingh0112@gmail.com>
2026-08-15 03:25:10 +0530

    feat: add health check endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './common/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
+import { HealthController } from './health.controller';
 
-@Module({ imports: [PrismaModule, TasksModule] })
+@Module({
+  imports: [PrismaModule, TasksModule],
+  controllers: [HealthController],
+})
 export class AppModule {}
